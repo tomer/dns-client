609aeb13691f3d2d72a47091619fa471fc8f6df5
tomer <tomer@users.noreply.github.com>
2020-07-04 16:49:10 +0000

    deploy: a90cfdc4c9cea7076c590ed50e3e18079c4b0577

---
+++ precache-manifest.ff40527d1e09c39a26a39379708eb283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c986e507b7d0d01cbc4abfa88cb7a9b",
+    "url": "/dns-client/index.html"
+  },
+  {
+    "revision": "2628ba6fdef8f6a7bafa",
+    "url": "/dns-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ba4192f1b61c2de51b41",
+    "url": "/dns-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2628ba6fdef8f6a7bafa",
+    "url": "/dns-client/static/js/2.bc03ac0d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dns-client/static/js/2.bc03ac0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4192f1b61c2de51b41",
+    "url": "/dns-client/static/js/main.d415348c.chunk.js"
+  },
+  {
+    "revision": "47a562f0cde1e19cad8d",
+    "url": "/dns-client/static/js/runtime-main.9aad4fd8.js"
+  }
+]);
